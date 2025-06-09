70a1266d2ae4ec869e11affe1b33d660a35309b4
eren <eren.turkoglu@epitech.eu>
2025-06-09 22:30:33 +0200

    add: eventlistener functions

---
+++ src/events.js
@@ -0,0 +1,20 @@
+
+// props: {
+//      on: {
+//      mouseover: () => console.log('almost yay!'),
+//      click: () => console.log('yay!') ,
+export function addEventListener(eventName, handler, el) {
+    el.addEventListener(eventName, handler);
+    return handler;
+}
+
+export function addEventListeners(listeners = {}, el) {
+    const addedListeners = {};
+
+    Object.entries(listeners).forEach(([eventName, handler]) => {
+        const listener = addEventListener(eventName, handler, el);
+        addedListeners[eventName] = listener;
+    });
+    return addedListeners;
+}
+
