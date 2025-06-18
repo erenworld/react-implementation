c116c1a556744b1b41fbd3e900e53ceb77cdb5f0
eren <eren.turkoglu@epitech.eu>
2025-06-18 10:28:55 +0200

    add: objetDiff and arraysDiff for comparing nodes on both VDOM

--- src/utils/arrays.js
+++ src/utils/arrays.js
@@ -2,6 +2,9 @@ export function withoutNulls(arr) {
     return arr.filter((item) => item != null); // != for null and undefined !
 }
 
-// instead of writing h('div', {}, [hString('Hello '), hString('world!')])
-// we can write h('div', {}, ['Hello ', 'world!'])
-
+export function arraysDiff(oldArr, newArr) {
+    return {
+        added: newArr.filter(newItem => !oldArr.includes(newItem)),
+        removed: oldArr.filter(oldItem => !newArr.includes(oldItem))
+    }
+}
