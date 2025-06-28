3eac2e2feb366fef9bc584c0593cb85e149dc93e
eren <eren.turkoglu@epitech.eu>
2025-06-28 15:20:36 +0200

    Add setClass helper

--- src/attributes.js
+++ src/attributes.js
@@ -5,7 +5,9 @@ export function setAttributes(el, attrs) {
         setClass(el, className);
     }
     if (style) {
-        Object.entries(style).forEach(([prop, value]) => {
+        // el, 'fontSize', '14px'..
+        // [['color', 'red'], ['fontSize', '14px']]
+        Object.entries(style).forEach(([prop, value]) => { 
             setStyle(el, prop, value)
         })
     }
@@ -14,7 +16,17 @@ export function setAttributes(el, attrs) {
     }
 }
 
-// TODOS: implement setClass
+// The classList property returns an object—a DOMTokenList
+function setClass(el, className) {
+    el.className = '';
+
+    if (typeof className === 'string') {
+        el.className = className;
+    }
+    if (Array.isArray(className)) {
+        el.classList.add(...className);
+    }
+}
 
 // TODOS: implement setStyle
 
