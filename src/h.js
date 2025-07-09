525f415320bb2e0e522e01123bc08208a41bdc06
eren <eren.turkoglu@epitech.eu>
2025-07-09 10:39:52 +0200

    Implement virtual DOM diffing and patching system

--- src/h.js
+++ src/h.js
@@ -29,3 +29,22 @@ export function hFragment(vNodes) {
     }
 }
 
+export function extractChildren(vdom) {
+    if (vdom.children == null) {
+        return [];
+    }
+
+    const children = [];
+
+    for (const child of vdom.children) {
+        if (child.type === DOM_TYPES.FRAGMENT) {
+            children.push(...extractChildren(child));
+        } else {
+            children.push(child);
+        }
+    }
+    
+    return children;
+}
+
+
--- src/patch-dom.js
+++ src/patch-dom.js
@@ -1,7 +1,17 @@
 import { destroyDOM } from './destroy-dom';
 import { mountDOM } from './mount-dom';
-import { DOM_TYPES } from './h';
+import { DOM_TYPES, extractChildren } from './h';
 import { areNodesEqual } from './nodes-equal';
+import { objectsDiff } from './utils/objects';
+import { arraysDiff, arraysDiffSequence, ARRAY_DIFF_OP } from './utils/arrays';
+import { isNotBlankOrEmptyString } from './utils/string';
+import {
+    setAttribute,
+    removeAttribute,
+    removeStyle,
+    setStyle
+} from './attributes';
+import { addEventListener } from './events';
 
 export function patchDOM(oldVdom, newVdom, parentEl) {
     if (!areNodesEqual(oldVdom, newVdom)) {
@@ -24,6 +34,7 @@ export function patchDOM(oldVdom, newVdom, parentEl) {
             break;
         }
     }
+    patchChildren(oldVdom, newVdom);
 
     return newVdom;
 }
@@ -36,6 +47,135 @@ function patchText(oldVdom, newVdom) {
     if (oldValue !== newValue) {
         el.nodeValue = newValue;
     }
+}
+
+function patchElement(oldVdom, newVdom) {
+    const el = oldVdom.el;
+    const {
+        class: oldClass,
+        style: oldStyle,
+        on: oldEvents,
+        ...oldAttrs
+    } = oldVdom.props;
+    const {
+        class: newClass,
+        style: newStyle,
+        on: newEvents,
+        ...newAttrs
+    } = newVdom.props;
+    const { listeners: oldListeners } = oldVdom;
+
+    patchAttrs(el, oldAttrs, newAttrs);
+    patchClasses(el, oldClass, newClass);
+    patchStyles(el, oldStyle, newStyle);
+    newVdom.listeners = patchEvents(el, oldListeners, oldEvents, newEvents);
+}
+
+function patchAttrs(el, oldAttrs, newAttrs) {
+    const { added, removed, updated } = objectsDiff(oldAttrs, newAttrs);
+
+    for (const attr of removed) {
+        removeAttribute(el, attr);
+    }
+    for (const attr of added.concat(updated)) {
+        setAttribute(el, attr, newAttrs[attr]);
+    }
+}
+
+function patchClasses(el, oldClass, newClass) {
+    const oldClasses = toClassList(oldClass);
+    const newClasses = toClassList(newClass);
+
+    const { added, removed } = arraysDiff(oldClasses, newClasses);
+    
+    if (removed.length > 0) {
+        el.classList.remove(...removed);
+    }
+    if (added.length > 0) {
+        el.classList.add(...added);
+    }
+}
+
+function patchStyles(el, oldStyle = {}, newStyle = {}) {
+    const { added, removed, updated } = objectsDiff(oldStyle, newStyle);
+
+    for (const style of removed) {
+        removeStyle(el, style);
+    }
+    for (const style of added.concat(updated)) {
+        setStyle(el, style, newStyle[style]);
+    }
+}
+
+function patchEvents(
+    el,
+    oldListeners = {},
+    oldEvents = {},
+    newEvents = {}
+) {
+    const { removed, added, updated } = objectsDiff(oldEvents, newEvents);
+
+    for (const eventName of removed.concat(updated)) {
+        el.removeEventListener(eventName, oldListeners[eventName]);
+    }
+
+    const addedListeners = {};
+    for (const eventName of added.concat(updated)) {
+        const listener = addEventListener(eventName, newEvents[eventName], el)
+        addedListeners[eventName] = listener;
+    }
+    return addedListeners;
+}
+
+function patchChildren(oldVdom, newVdom) {
+    // Extracts the old children array or uses an empty array
+    const oldChildren = extractChildren(oldVdom); 
+    const newChildren = extractChildren(newVdom);
+    const parentEl = oldVdom.el;
+
+    // Finds the operation to transform the old array into new one
+    const diffSeq = arraysDiffSequence(
+        oldChildren,
+        newChildren,
+        areNodesEqual
+    )
+
+    for (const operation of diffSeq) {
+        const { originalIndex, index, item } = operation;
+
+        switch (operation.op) {
+            case ARRAY_DIFF_OP.ADD: {
+                mountDOM(item, parentEl, index);
+                break;
+            }
+            case ARRAY_DIFF_OP.REMOVE: {
+                destroyDOM(item);
+                break;
+            }
+            case ARRAY_DIFF_OP.MOVE: {
+                const oldChild = oldChildren[originalIndex];
+                const newChild = newChildren[index];
+                const el = oldChild.el;
+                const elAtTargetIndex = parentEl.childNodes[index];
+
+                parentEl.insertBefore(el, elAtTargetIndex);
+                patchDOM(oldChild, newChild, parentEl);
+
+                break;
+            }
+            case ARRAY_DIFF_OP.NOOP: {
+                patchDOM(oldChildren[originalIndex], newChildren[index], parentEl);
+                break;
+            }
+        }
+    }
+}
+
+function toClassList(classes = '') {
+    return Array.isArray(classes)
+        ? classes.filter(isNotBlankOrEmptyString) // Filters blank and empty strings
+        : classes.split(/(\s+)/)
+            .filter(isNotBlankOrEmptyString) // Splits the string on whitespace and filters blank and empty strings
 }
 
 function findIndexInParent(parentEl, el) {
