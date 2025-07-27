1b55768c74520636aaf35ece87d60c256f133711
eren <eren.turkoglu@epitech.eu>
2025-07-27 17:52:21 +0200

    Added prettier formatter

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-import { RouterLink } from './routing/router-component.js';
-
 export { createApp } from './app';
 export { DOM_TYPES, hString, hElement, hFragment, hSlot } from './h';
 export { defineComponent } from './components/component';
